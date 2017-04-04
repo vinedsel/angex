8b118bd165389f755abc618692fbc7e6ae683147
Raymond Gregory <vinedsel@gmail.com>
2017-04-04 02:53:58 -0500

    Modified these select files.

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
 var MyApp;
 (function (MyApp) {
     angular.module("MyApp", []);
-    var HomeController = (function () {
-        function HomeController() {
-            this.friends = ["Cartman", "Kyle", "Timmy", "Stan", "Kenny"];
-        }
-        return HomeController;
-    }());
-    angular.module("MyApp").controller("homeController", HomeController);
 })(MyApp || (MyApp = {}));
--- app.ts
+++ app.ts
@@ -1,16 +1,5 @@
 namespace MyApp {
 
-    angular.module("MyApp", []);
+  angular.module("MyApp", []);
 
-
-    class HomeController {
-
-        public friends;
-
-        constructor() {
-          this.friends=["Cartman", "Kyle", "Timmy", "Stan", "Kenny"];
-        }
-    }
-
-    angular.module("MyApp").controller("homeController", HomeController);
 }
